52e832b5f443ec400e9c8d33ad0cd8d707353d5d
Jiří Třečák <jiri@supernova.studio>
2021-04-28 13:38:18 +0200

    #documentation - Code block, code highlight and safe encoding

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,13 +3,13 @@
 
 Pulsar.registerFunction("pageUrl", pageUrl)
 Pulsar.registerFunction("assetUrl", assetUrl)
+Pulsar.registerFunction("highlightSafeString", highlightSafeString)
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-// MARK: - Page manipulation
+// MARK: - URLs
 
 /** Generate page slug for the generated page */
 function pageUrl(page: DocumentationPage, prefix: string | undefined) {
-
   let pageSlug = page.userSlug ?? page.slug
   let subpaths: Array<string> = []
 
@@ -30,7 +30,6 @@ function pageUrl(page: DocumentationPage, prefix: string | undefined) {
 
 /** Create proper url that changes with the folder-depth of the documentation */
 function assetUrl(asset: string, prefix: string | undefined) {
-
   let assetFolder = "assets"
   let fragments = [prefix, assetFolder, asset]
 
@@ -39,9 +38,65 @@ function assetUrl(asset: string, prefix: string | undefined) {
   return path
 }
 
-
-/** Retrieve safe slag name made out of any string
- */
+/** Retrieve safe slag name made out of any string */
 function slugName(name: string) {
   return name.replace(/\W+/g, "-").toLowerCase()
+}
+
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+// MARK: - String manipulation
+
+/** Escape special characters in the given string of text. Encoding part taken from https://github.com/component/escape-html */
+function highlightSafeString(block: DocumentationPageBlockCode) {
+  // Retrieve raw text, ignore all attributes for now
+  let string = block.text.spans.map((s) => s.text).join("")
+
+  // Make sure it is properly safe for HTML rendering
+  return escapeHtml(string)
+}
+
+function escapeHtml(string) {
+  var matchHtmlRegExp = /["'&<>]/
+  var str = "" + string
+  var match = matchHtmlRegExp.exec(str)
+
+  if (!match) {
+    return str
+  }
+
+  var escape
+  var html = ""
+  var index = 0
+  var lastIndex = 0
+
+  for (index = match.index; index < str.length; index++) {
+    switch (str.charCodeAt(index)) {
+      case 34: // "
+        escape = "&quot;"
+        break
+      case 38: // &
+        escape = "&amp;"
+        break
+      case 39: // '
+        escape = "&#39;"
+        break
+      case 60: // <
+        escape = "&lt;"
+        break
+      case 62: // >
+        escape = "&gt;"
+        break
+      default:
+        continue
+    }
+
+    if (lastIndex !== index) {
+      html += str.substring(lastIndex, index)
+    }
+
+    lastIndex = index + 1
+    html += escape
+  }
+
+  return lastIndex !== index ? html + str.substring(lastIndex, index) : html
 }
