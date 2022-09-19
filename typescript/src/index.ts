0705103036d94d45d3cf717b15228c6f3f622628
Jiri Trecak <jiritrecak@gmail.com>
2022-09-19 18:50:51 +0200

    #styling - Added option to link class for specific page

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -11,7 +11,7 @@ import { htmlSafeString, htmlSafeUrl } from "./doc_functionality/sandbox"
 import { buildSearchIndexJSON } from "./doc_functionality/search"
 import { highlightSafeString, withHTMLNewlines, getUrlExtension } from "./doc_functionality/string_utils"
 import { formattedTokenGroupHeader, fullTokenGroupName, gradientDescription, gradientTokenValue, measureTypeIntoReadableUnit, scaledShadowTokenValue, shadowDescription, shadowTokenValue, typographyDescription } from "./doc_functionality/tokens"
-import { assetUrl, textBlockPlainText, pageUrl, rootUrl, slugifyHeading } from "./doc_functionality/urls"
+import { assetUrl, textBlockPlainText, pageUrl, rootUrl, slugifyHeading, pageIdentifier } from "./doc_functionality/urls"
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Blueprint functions
@@ -35,6 +35,7 @@ Pulsar.registerFunction("buildSearchIndexJSON", buildSearchIndexJSON)
 
 /* URL manipulation and support */
 Pulsar.registerFunction("pageUrl", pageUrl)
+Pulsar.registerFunction("pageIdentifier", pageIdentifier)
 Pulsar.registerFunction("rootUrl", rootUrl)
 Pulsar.registerFunction("assetUrl", assetUrl)
 Pulsar.registerFunction("slugifyHeading", slugifyHeading)
