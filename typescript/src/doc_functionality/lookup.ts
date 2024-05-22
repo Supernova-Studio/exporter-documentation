5072dc56ede682d8dd9e4203125c07f259881b01
Matej Bobek <matejbobek@sudolabs.io>
2024-05-22 21:51:24 +0200

    Revert "Add debug for configuration"

--- typescript/src/doc_functionality/lookup.ts
+++ typescript/src/doc_functionality/lookup.ts
@@ -1,81 +1,73 @@
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Imports
 
-import { htmlSafeString } from './sandbox';
+import { htmlSafeString } from "./sandbox"
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Support
 
 /** Retrieve first subgroup below root group */
 export function firstSubgroupOfPage(page: DocumentationPage) {
-  let parent: DocumentationGroup | null = page.parent;
+  let parent: DocumentationGroup | null = page.parent
   while (true) {
     if (!parent || parent.isRoot) {
-      return undefined;
+      return undefined
     }
 
     if (parent.parent && parent.parent.isRoot) {
-      return parent;
+      return parent
     }
-    parent = parent.parent;
+    parent = parent.parent
   }
 }
 
 /** Check whether targeted item is descendant of provided root */
-export function pageOrGroupActiveInContext(
-  pageOrGroup: DocumentationPage | DocumentationGroup,
-  context: DocumentationPage | DocumentationGroup
-) {
-  if (context.type === 'Page') {
+export function pageOrGroupActiveInContext(pageOrGroup: DocumentationPage | DocumentationGroup, context: DocumentationPage | DocumentationGroup) {
+  if (context.type === "Page") {
     // If we are checking against plain page, then we can only compare
-    let contextPage = context as DocumentationPage;
-    return contextPage.id === pageOrGroup.id;
+    let contextPage = context as DocumentationPage
+    return contextPage.id === pageOrGroup.id
   } else {
     // If we are checking against group, check everything upwards the tree. If group contains the page, return that information
-    let contextGroup = context as DocumentationGroup;
+    let contextGroup = context as DocumentationGroup
     if (contextGroup.childrenIds.indexOf(pageOrGroup.persistentId) !== -1) {
-      return true;
+      return true
     } else if (pageOrGroup.parent) {
-      return pageOrGroupActiveInContext(pageOrGroup.parent, contextGroup);
+      return pageOrGroupActiveInContext(pageOrGroup.parent, contextGroup)
     } else {
       // Reached root and didn't find anything, abandon ship
-      return false;
+      return false
     }
   }
 }
 
 /** Find first showable page from the top of the provided root */
-export function firstPageFromTop(
-  documentationRoot: DocumentationGroup
-): DocumentationPage | null {
+export function firstPageFromTop(documentationRoot: DocumentationGroup): DocumentationPage | null {
+
   for (let child of documentationRoot.children) {
     if (isExportable(child as DocumentationPage | DocumentationGroup)) {
-      if (child.type === 'Page') {
-        return child as DocumentationPage;
+      if (child.type === "Page") {
+        return child as DocumentationPage
       } else {
-        let possiblePage = firstPageFromTop(child as DocumentationGroup);
+        let possiblePage = firstPageFromTop(child as DocumentationGroup)
         if (possiblePage) {
-          return possiblePage;
+          return possiblePage
         }
       }
     }
   }
-  return null;
+  return null
 }
 
+
 /** Get all children of the page if the homepage is tabs */
-export function firstTabGroupFromTop(
-  documentationRoot: DocumentationGroup
-): string[] | undefined {
+export function firstTabGroupFromTop(documentationRoot: DocumentationGroup): string[] | undefined {
+
   let pageCount = 0;
   for (let child of documentationRoot.children) {
     if (isExportable(child as DocumentationPage | DocumentationGroup)) {
-      if (
-        pageCount === 0 &&
-        child.type === 'Group' &&
-        (child as DocumentationGroup).groupBehavior === 'Tabs'
-      ) {
-        return (child as DocumentationGroup).childrenIds;
+      if (pageCount === 0 && child.type === "Group" && (child as DocumentationGroup).groupBehavior === "Tabs") {
+        return (child as DocumentationGroup).childrenIds
       }
       pageCount++;
     }
@@ -84,10 +76,8 @@ export function firstTabGroupFromTop(
 }
 
 /** Check if the page is part of the homepage (if homepage is tabs) */
-export function isHomepageTab(
-  page: DocumentationPage,
-  documentationRoot: DocumentationGroup
-): boolean {
+export function isHomepageTab(page: DocumentationPage, documentationRoot: DocumentationGroup): boolean {
+  
   const homepageTabs = firstTabGroupFromTop(documentationRoot);
   if (homepageTabs?.includes(page.persistentId)) {
     return true;
@@ -97,12 +87,10 @@ export function isHomepageTab(
 }
 
 /** Check if the page is a homepage */
-export function isHomepage(
-  page: DocumentationPage,
-  documentationRoot: DocumentationGroup
-): boolean {
-  let homepagePage = firstPageFromTop(documentationRoot);
-  if (homepagePage !== null && page.id === homepagePage.id) {
+export function isHomepage(page: DocumentationPage, documentationRoot: DocumentationGroup): boolean {
+  
+  let homepagePage = firstPageFromTop(documentationRoot)
+  if ((homepagePage !== null && page.id === homepagePage.id)) {
     return true;
   }
 
@@ -110,12 +98,8 @@ export function isHomepage(
 }
 
 /** Resolve menu label */
-export function resolveMenuLabel(
-  page: DocumentationPage,
-  documentationRoot: DocumentationGroup,
-  overridenLabel: string
-): string {
-  if (isHomepage(page, documentationRoot) && overridenLabel !== '') {
+export function resolveMenuLabel(page: DocumentationPage, documentationRoot: DocumentationGroup, overridenLabel: string): string {
+  if (isHomepage(page, documentationRoot) && overridenLabel !== "") { 
     return overridenLabel;
   } else {
     return htmlSafeString(page.title);
@@ -123,85 +107,82 @@ export function resolveMenuLabel(
 }
 
 /** Create flattened structure of pages */
-export function flattenedPageStructure(
-  root: DocumentationGroup
-): Array<DocumentationPage> {
-  let pages: Array<DocumentationPage> = [];
+export function flattenedPageStructure(root: DocumentationGroup): Array<DocumentationPage> {
+
+  let pages: Array<DocumentationPage> = []
   for (let item of root.children) {
     if (isExportable(item as DocumentationPage | DocumentationGroup)) {
-      if (item.type === 'Page') {
-        pages.push(item as DocumentationPage);
-      } else if (item.type === 'Group') {
-        pages = pages.concat(
-          flattenedPageStructure(item as DocumentationGroup)
-        );
+      if (item.type === "Page") {
+        pages.push(item as DocumentationPage)
+      } else if (item.type === "Group") {
+        pages = pages.concat(flattenedPageStructure(item as DocumentationGroup))
       }
     }
   }
 
-  return pages;
+  return pages
 }
 
 /** Find next page after provided page */
-export function nextPage(
-  page: DocumentationPage,
-  documentationRoot: DocumentationGroup
-): DocumentationPage | null {
-  let flattenedPages = flattenedPageStructure(documentationRoot);
-  let pageIndex = flattenedPages.findIndex(p => p.id === page.id);
+export function nextPage(page: DocumentationPage, documentationRoot: DocumentationGroup): DocumentationPage | null {
+
+  let flattenedPages = flattenedPageStructure(documentationRoot)
+  let pageIndex = flattenedPages.findIndex(p => p.id === page.id)
   if (pageIndex !== -1) {
     if (pageIndex < flattenedPages.length - 1) {
-      return flattenedPages[pageIndex + 1];
+      return flattenedPages[pageIndex + 1]
     }
   }
-  return null;
+  return null
 }
 
 /** Find previous page of provided page */
-export function previousPage(
-  page: DocumentationPage,
-  documentationRoot: DocumentationGroup
-): DocumentationPage | null {
-  let flattenedPages = flattenedPageStructure(documentationRoot);
-  let pageIndex = flattenedPages.findIndex(p => p.id === page.id);
+export function previousPage(page: DocumentationPage, documentationRoot: DocumentationGroup): DocumentationPage | null {
+
+  let flattenedPages = flattenedPageStructure(documentationRoot)
+  let pageIndex = flattenedPages.findIndex(p => p.id === page.id)
   if (pageIndex > 0) {
-    return flattenedPages[pageIndex - 1];
+    return flattenedPages[pageIndex - 1]
   }
-  return null;
+  return null
 }
 
 /** Check whether page or group is exportable. Currently page is considered exportable if it doesn't start with underscore */
-export function isExportable(
-  object: DocumentationPage | DocumentationGroup
-): boolean {
+export function isExportable(object: DocumentationPage | DocumentationGroup): boolean {
+
   if (object === null || object === undefined) {
-    return false;
+    return false
   }
-  if (object.type === 'Group') {
-    let parent = (object as DocumentationGroup).parent;
-    if (parent) {
-      return isExportable(parent);
+  if (object.type === "Group") {
+    if (object.title.startsWith("_")) {
+      return false
     } else {
-      // Reached root structure without any group being non-exportable, so the entire structure is exportable
-      return true;
+      let parent = (object as DocumentationGroup).parent
+      if (parent) {
+        return isExportable(parent)
+      } else {
+        // Reached root structure without any group being non-exportable, so the entire structure is exportable
+        return true
+      }
     }
-  } else if (object.type === 'Page') {
-    return true;
+  } else if (object.type === "Page") {
+    return !object.title.startsWith("_")
   } else {
-    return false;
+    return false
   }
 }
 
-/** Get current timestamp,
+/** Get current timestamp, 
  *  e.g. to version custom.css with the date of publishing
  */
 export function getCurrentTimestamp(): string {
   return Math.floor(Date.now()).toString();
 }
 
+
 /** Check if the key is in the array of keys
  */
-export function checkKeyInArray(array, key: string): boolean {
+ export function checkKeyInArray(array, key: string): boolean {
   if (array) {
     for (let i = 0; i < array.length; i++) {
       if (array[i] === key) {
