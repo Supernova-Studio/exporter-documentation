f7ee37e90c76e151a6ea1da6e675a18053167833
Matej Bobek <matejbobek@sudolabs.io>
2024-09-23 10:01:53 +0200

    Add comment

--- typescript/src/doc_functionality/themes.ts
+++ typescript/src/doc_functionality/themes.ts
@@ -1,5 +1,6 @@
 import { escapeHtml } from './string_utils';
 
+/** Returns a string of themes separated by <br /> used for the tooltip */
 export function getThemesTooltip(themes: Array<{ name: string }>) {
   // Escape HTML to prevent html content in the tooltip
   return themes.map(t => escapeHtml(t.name)).join('<br />');
