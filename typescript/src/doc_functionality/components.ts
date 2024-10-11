522a406bc1ffc64088b8593fd4734948c9405333
Matej Bobek <matejbobek@sudolabs.io>
2024-10-11 15:37:45 +0200

    Add support for multiple selected components to the util

--- typescript/src/doc_functionality/components.ts
+++ typescript/src/doc_functionality/components.ts
@@ -15,24 +15,141 @@ type ComponentPreview = {
   })[];
 };
 
+type DesignComponentVariant = DesignComponent['componentPropertyDefinitions'] & {
+  value: string;
+};
+
+/** If variant has all values that are default, consider it as a default variant */
+function getDefaultVariantPreview(
+  selectedComponent: DesignComponent,
+  componentVariant: DesignComponent,
+  componentProperties: DesignComponentProperty[]
+) {
+  const isDefaultVariant = componentProperties.every(
+    definition =>
+      componentVariant.variantPropertyValues?.[definition.id] ===
+      definition.defaultValue
+  );
+
+  if (isDefaultVariant) {
+    return {
+      component: componentVariant,
+      name: selectedComponent.name,
+      properties: []
+    };
+  }
+
+  return null;
+}
+
+/** Returns all variant previews that should be rendered based on the selected properties */
+function getVariantPreviewsToRender(
+  selectedComponent: DesignComponent,
+  componentProperties: DesignComponentProperty[],
+  selectedPropertiesSet: Set<string>
+) {
+  const componentPropertyMap = new Map(
+    componentProperties.map(property => [property.id, property])
+  );
+
+  return (
+    selectedComponent.subcomponents?.reduce<ComponentPreview[]>(
+      (acc, componentVariant) => {
+        if (!componentVariant.variantPropertyValues) {
+          return acc;
+        }
+
+        const properties = Object.entries(
+          componentVariant.variantPropertyValues
+        ).flatMap(([id, value]) => {
+          const property = componentPropertyMap.get(id);
+
+          if (!property) {
+            return [];
+          }
+
+          return {
+            ...property,
+            value
+          };
+        });
+
+        if (selectedPropertiesSet.size === 0) {
+          // return default variant when no property ids are selected
+          const defaultVariantPreview = getDefaultVariantPreview(
+            selectedComponent,
+            componentVariant,
+            componentProperties
+          );
+
+          if (defaultVariantPreview) {
+            acc.push(defaultVariantPreview);
+          }
+
+          return acc;
+        }
+
+        if (
+          shouldShowVariant(
+            componentProperties,
+            componentVariant,
+            selectedPropertiesSet
+          )
+        ) {
+          acc.push({
+            component: componentVariant,
+            name: selectedComponent.name,
+            // We only want to show properties that are selected
+            properties: properties.filter(property =>
+              selectedPropertiesSet.has(property.id)
+            )
+          });
+        }
+
+        return acc;
+      },
+      []
+    ) ?? []
+  );
+}
+
+/** Returns true if the variant should be shown based on the selected properties */
+function shouldShowVariant(
+  componentProperties: DesignComponentProperty[],
+  componentVariant: DesignComponent,
+  selectedPropertiesSet: Set<string>
+): boolean {
+  return componentProperties.every(componentProperty => {
+    const componentPropertyVariantValue =
+      componentVariant.variantPropertyValues?.[componentProperty.id];
+    if (selectedPropertiesSet.has(componentProperty.id)) {
+      // if the property is selected, return true if it has any value
+      return componentPropertyVariantValue;
+    }
+
+    // if the property is not selected, check if it has default value
+    return componentPropertyVariantValue === componentProperty.defaultValue;
+  });
+}
+
 export function getComponentPreviews(
   selectedComponent: DesignComponent,
-  selectedPropertyIds: string[],
-  areSelectedMultipleComponents: boolean
+  selectedPropertyIds: string[]
 ): ComponentPreview[] {
+  const selectedProperties = selectedPropertyIds ?? [];
+
+  if (!selectedComponent) {
+    return [];
+  }
+
+  // use a set for faster lookups
+  const selectedPropertiesSet = new Set(selectedProperties);
   const componentProperties = Object.values(
-    selectedComponent.componentPropertyDefinitions
-  )
-    ?.filter(
-      // we want to find variants only for variant properties definitions
-      definition => definition.type === 'Variant'
-    )
-    ?.map(propertyDefinition => {
-      return {
-        ...propertyDefinition,
-        isSelected: selectedPropertyIds.includes(propertyDefinition.id)
-      };
-    });
+    selectedComponent.componentPropertyDefinitions ?? {}
+  ).filter(
+    // we want to find variants only for variant properties definitions
+    definition => definition.type === 'Variant'
+  );
 
   if (!componentProperties || componentProperties.length === 0) {
     // render selected component when it has no properties
@@ -45,83 +162,11 @@ export function getComponentPreviews(
     ];
   }
 
-  const componentPropertyMap =
-    componentProperties.reduce<
-      Record<string, DesignComponentProperty>
-    >((acc, property) => {
-      acc[property.id] = property;
-      return acc;
-    }, {}) ?? {};
-
-  const variantsToRender =
-    selectedComponent.subcomponents?.reduce<ComponentPreview[]>(
-      (acc, componentVariant) => {
-        if (!componentVariant.variantPropertyValues) {
-          return acc;
-        }
-
-        const properties = Object.entries(
-          componentVariant.variantPropertyValues
-        ).flatMap(([id, value]) => {
-          const property = componentPropertyMap[id];
-
-          if (!property) {
-            return [];
-          }
-
-          return {
-            ...property,
-            value
-          };
-        });
-
-        if (areSelectedMultipleComponents || selectedPropertyIds.length === 0) {
-          // render default variants when multiple components are selected or no property ids are selected. If variant has all values that are default, consider it as a default variant
-          const isDefaultVariant = componentProperties.every(
-            definition =>
-              componentVariant.variantPropertyValues?.[definition.id] ===
-              definition.defaultValue
-          );
-
-          if (isDefaultVariant) {
-            acc.push({
-              component: componentVariant,
-              name: selectedComponent.name,
-              properties
-            });
-          }
-
-          return acc;
-        }
-
-        const shouldShowVariantBasedOnPropertySelection = componentProperties?.every(
-          componentProperty => {
-            const componentPropertyVariantValue =
-              componentVariant.variantPropertyValues?.[componentProperty.id];
-            if (componentProperty.isSelected) {
-              return componentPropertyVariantValue;
-            }
-
-            // if the property is not selected, check if it has default value
-            return (
-              componentPropertyVariantValue === componentProperty.defaultValue
-            );
-          }
-        );
-
-        if (shouldShowVariantBasedOnPropertySelection) {
-          acc.push({
-            component: componentVariant,
-            name: selectedComponent.name,
-            // return only properties that are selected
-            properties: properties.filter(property => selectedPropertyIds.includes(property.id))
-          });
-        }
-
-        return acc;
-      },
-      []
-    ) ?? [];
+  const variantsToRender = getVariantPreviewsToRender(
+    selectedComponent,
+    componentProperties,
+    selectedPropertiesSet
+  );
 
   if (variantsToRender.length === 0) {
     // there are no variants to render so render selected component
